7370c5380991eed34320dda02e4ccfd0230aad26
Katy Farmer <dreadpiratekaty@gmail.com>
2017-05-22 17:33:46 -0600

    Typo on homepage

---
+++ spec/features/mark_links_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+feature "can mark links as read", :js => :true do
+  let(:user) { Fabricate(:user) }
+  let!(:link) { Fabricate(:link, user: user) }
+
+  scenario "Mark a link as read" do
+    login(user)
+
+    within('.links .read-status') do
+      expect(page).to have_text("false")
+    end
+
+    click_on "Mark as Read"
+
+    within('.links .read-status') do
+      expect(page).to have_text("true")
+    end
+
+  end
+end
+
+# feature "can mark links as unread", :js => true do
+#   let(:user) { Fabricate(:user) }
+#   let!(:link) { Fabricate(:link, user: user, read: true) }
+#
+#   scenario "Mark a link as read" do
+#     login(user)
+#
+#     within('.links .read-status') do
+#       expect(page).to have_text("true")
+#     end
+#
+#     click_on "Mark as Read"
+#
+#     within('.links .read-status') do
+#       expect(page).to have_text("false")
+#     end
+#
+#   end
+# end
